490be433caa97e449d19ad5513ba892cc5661248
Tomris Teymurlu <tomristt9@gmail.com>
2024-12-11 11:39:19 +0400

    removed springbootannotation

--- andersen-homework/src/main/java/com/example/ticket/TicketApplication.java
+++ andersen-homework/src/main/java/com/example/ticket/TicketApplication.java
@@ -2,10 +2,10 @@ package com.example.ticket;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication
+
+
 public class TicketApplication {
 
     public static void main(String[] args) {
