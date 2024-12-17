8c4a9cb4824a2eb5818de3b0052b83b1e716956e
Tomris Teymurlu <tomristt9@gmail.com>
2024-12-17 14:52:00 +0400

    deleted spring boot dependency

--- andersen-homework/src/main/java/com/example/ticket/TicketApplication.java
+++ andersen-homework/src/main/java/com/example/ticket/TicketApplication.java
@@ -9,6 +9,6 @@ import org.springframework.boot.SpringApplication;
 public class TicketApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TicketApplication.class, args);
+        System.out.println("Hello World!");
     }
 }
